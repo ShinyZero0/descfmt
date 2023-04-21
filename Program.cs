22c186e64147d8813691a144ccbed093df517dec
ShinyZero0 <shinyzero0@gmail.com>
2023-04-22 02:20:21 +0300

    DRY

--- Program.cs
+++ Program.cs
@@ -16,14 +16,7 @@
             int lastAngle = raw.LastIndexOf(">");
             if (firstAngle != -1 && lastAngle != -1)
             {
-                string[] typeAndName = raw.Substring(0, firstAngle).Split(':');
-                if (typeAndName.Length == 1)
-                    _type = typeAndName[0];
-                else
-                {
-                    _name = typeAndName[0].Trim();
-                    _type = typeAndName[1].Trim();
-                }
+                _setTypeAndName(raw.Substring(0, firstAngle).Split(':'));
                 foreach (
                     string entry in raw.Substring(firstAngle + 1, lastAngle - firstAngle - 1)
                         .Split(',')
@@ -34,15 +27,23 @@
             }
             else
             {
-                string[] typeAndName = raw.Split(':');
-                if (typeAndName.Length == 1)
-                    _type = typeAndName[0].Trim();
-                else
-                {
-                    _name = typeAndName[0].Trim();
-                    _type = typeAndName[1].Trim();
-                }
+                _setTypeAndName(raw.Split(':'));
             }
+        }
+
+        void _setTypeAndName(string[] typeAndName)
+        {
+            if (typeAndName.Length == 1)
+                _type = typeAndName[0];
+            else
+            {
+                _name = typeAndName[0];
+                _type = typeAndName[1];
+            }
+
+            if (_name != null)
+                _name = _name.Trim();
+            _type = _type!.Trim();
         }
 
         string? _name;
@@ -52,23 +53,28 @@
         public override string ToString()
         {
             List<string> output = new();
+
             if (_name != null)
                 output.Add($"{_name}: {_type}");
             else
                 output.Add(_type);
+
             if (_subTypes.Count > 2)
             {
                 output[0] = $"{output[0]}<";
                 _subTypes.ForEach(subtype => output.Add($"    {subtype.ToString()}"));
                 output.Add(">");
             }
-            else if (_subTypes.Count != 0)
+            else if (_subTypes.Count > 0)
+            {
                 output[output.Count - 1] = String.Concat(
                     output[output.Count - 1],
                     "<",
                     (String.Join(", ", _subTypes)),
                     ">"
                 );
+            }
+
             return String.Join('\n', output);
         }
     }
